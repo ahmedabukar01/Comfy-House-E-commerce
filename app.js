627b8d7b375a0c4e0743dadb708a22d5cdbf7f24
ahmed <ahmedlearn01@gmail.com>
2021-08-29 19:37:29 -0700

    contentful api

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const client = contentful.createClient({
+    // This is the space ID. A space is like a project folder in Contentful terms
+    space: "developer_bookshelf",
+    // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+    accessToken: "0b7f6x59a0"
+  });
+
+// variables
 const cartBtn = document.querySelector('.cart-btn');
 const closeCartBtn = document.querySelector('.close-cart');
 const clearCartBtn = document.querySelector('.clear-cart');
